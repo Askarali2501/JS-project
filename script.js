2d25cdeb9d6ee18c2ee1f67935c8a1623bf9a9c6
Oscar <exaple@gmail.com>
2022-11-24 09:51:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const timerId = setTimeout(function(text) {
+    console.log(text);
+}, 2000, 'Hello');
+
+const btn = document.querySelector('.btn')
+
+function myAnimation() {
+    const elem = document.querySelector('.box');
+}
